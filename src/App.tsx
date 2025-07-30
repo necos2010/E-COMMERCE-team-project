24cbedab77050c403f128cd7c5eaec6c16e0f276
Mirumar <bosithabibullaev@gmail.com>
2025-07-30 15:32:40 +0500

    Made Sign and Logipages

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ const App: React.FC = () => {
           element: <User />,
         },
         {
-          path: "log-in",
+          path: "/login",
           element: <LogIn />,
         },
         {
--- src/features/logIn/Index.tsx
+++ src/features/logIn/Index.tsx
@@ -1,17 +1,69 @@
-import SideImg from "../../assets/Side-img.png";
+import { useState, ChangeEvent, FC } from 'react';
+import styles from './LogIn.module.css';
+import SideImg from '../../assets/Side-img.png';
 
-function Index() {
+interface FormData {
+  name: string;
+  contact: string;
+  password: string;
+}s
+
+const LogIn: FC = () => {
+  const [form, setForm] = useState<FormData>({
+    name: '',
+    contact: '',
+    password: '',
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
-    <div>
-      <div className="login-main-wrapper">
-        <img src={SideImg} alt="" />
-        <div className="login-word-wrapper">
-          <h2>Create an account</h2>
-          <p>Enter your details below</p>
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <img src={SideImg} alt="Shopping" />
+      </div>
+
+      <div className={styles.right}>
+        <h2>Log in to Exclusive</h2>
+        <p>Enter your details below</p>
+
+        <div className={styles.formContainer}>
+
+          <div className={styles.inputGroup}>
+            <input
+              type="text"
+              name="contact"
+              value={form.contact}
+              onChange={handleChange}
+              required
+              placeholder=" "
+            />
+            <label>Email or Phone Number</label>
+          </div>
+
+          <div className={styles.inputGroup}>
+            <input
+              type="password"
+              name="password"
+              value={form.password}
+              onChange={handleChange}
+              required
+              placeholder=" "
+            />
+            <label>Password</label>
+          </div>
+<div className={styles.buttonContainer}>
+  <button className={styles.loginBtn}>Log In</button>
+  <button className={styles.forgetPasswordBtn}>Forget Password?</button>
+</div>
+
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default LogIn;
